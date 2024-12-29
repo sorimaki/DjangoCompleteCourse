fbb38f9d222c8b0e781667b4a662f602c74eeac2
Sorin <sorinsystems@gmail.com>
2024-12-29 16:49:53 -0500

    Part 4 - Database implementation

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 #from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from .models import Post
+from django.shortcuts import get_object_or_404 
 
-
+"""
 posts = [
     {
         'id':1,
@@ -21,11 +23,13 @@ posts = [
         'description': 'Title 3 Description',
     },
 ]
-
+"""
 #Sposts = []
 
 # Create your views here.
-def home(request):
+"""
+def home(request): 
+Old home code 
     html = ""
 
     for post in posts:
@@ -37,9 +41,8 @@ def home(request):
             </div>
 '''
     return render(request, 'posts/index.html', {'name':'BUBU', 'list':posts} ) 
-
 def post(request, id):
-
+Old post code
     #print(type(id))
     valid_id = False
     for post in posts:
@@ -59,7 +62,21 @@ def post(request, id):
         return render(request, 'posts/post.html', {'post_dict':post_dict})
     else:
         # return HttpResponseNotFound("These are not the droids you are looking for")
-        raise Http404()
+        raise Http404()    
+"""
+
+def home(request):
+    all_posts = Post.objects.all()
+    #print(all_posts)
+    return render(request, 'posts/index.html', {'name':'BUBU', 'list':all_posts} ) 
+
+def post(request, id):
+    """try:
+        post = Post.objects.get(id=id)
+    except:
+        raise Http404()"""
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'posts/post.html', {'post_dict':post})
 
 def google(request, id):
     url = reverse('post', args=[id])
